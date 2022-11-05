3d134a7fc346cc7613d4491afe3c7268995bba7e
Yvan Quinn <yvan.qu@gmail.com>
2022-11-05 17:31:17 -0500

    incorporated audio and window views to main

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import RandPerson
+from playsound import playsound
+import time
 
 def OpenScript():
     print("Hello! This is a text based real time train simulation made by Kelly Sun and Yvan Quinn.")
     print("The views out the window and sounds are based in real time and real life.")
-    print("Hello! You are currently at Toyosu Station, a terminal stop of the Yurikamome Line.\n The New Transit Yurikamome is a driverless, automated transit service linking Shimbashi to Toyosu, via Odaiba across the Rainbow Bridge in Tokyo.\nYou are on your way to visit your grandma, so you'll want to get off at Shimbashi station.")
+    print("Hello! You are currently at Toyosu Station, a terminal stop of the Yurikamome Line.\nThe New Transit Yurikamome is a driverless, automated transit service linking Shimbashi to Toyosu, via Odaiba across the Rainbow Bridge in Tokyo.\nYou are on your way to visit Grandma, so you'll want to get off at Shimbashi station.")
 
 def GetOnTrain():
     print("The train has arrived, it brings about a blast of wind as it slows to a stop at the station.  The automatic doors open with a whoosh.")
@@ -13,8 +15,17 @@ def GetOnTrain():
             break
         else:
             print("The automatic doors closes with a snap, and the train begins to leave the station.")
-            print("You bailed on grandma. \nGAME OVER.")
+            print("You bailed on Grandma.\nGAME OVER.")
             exit()
+    playsound('audio.m4a', False)
+    start = time.time()
+    while(time.time() - start < 2000):
+        Interaction()
+    print("You missed your stop, the train has turned arround. You'll spend the next half our riding the train home in shame.")
+    print("Grandma will be very disappointed.\nGAME OVER>")
+    exit()
+
+
 
     
 
@@ -26,6 +37,10 @@ def Interaction():
     print("what would you like to do?")
     print()
 
+
+def windowView()
+    print(open("ascii_stills/"+str(int((time.time()-start)/10)*10)+".txt").read())
+
 def main():
     OpenScript()
     GetOnTrain()
@@ -33,4 +48,4 @@ def main():
 
     
 
-
+main()
