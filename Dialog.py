cc339523821060019b2dd7f39f1cccc3af6ce9e1
Yvan Quinn <yvan.qu@gmail.com>
2022-11-05 20:37:15 -0500

    fix bug

--- Dialog.py
+++ Dialog.py
@@ -14,9 +14,9 @@ def Dialog(start_time):
             print("The stranger was staring blankly out of the window, as if they were a thousand miles away. \nAt your greeting they blink and stare at you startled.")
             username = input("Oh...Hi, where are my manners. I'm sorry about that. My name is " + name + ".  What is your name? \n")
             visit = input("Nice to meet you" + username + ". Are you visiting someone? \n")
-            if visit.lower == "yes":
+            if visit.lower() == "yes":
                 yesvisit = input("Oh, who are you visiting? \n")
-                if "grandma" in yesvisit.lower:
+                if "gran" in yesvisit.lower():
                     print(name + "choked, oh... I'm on my way to visit mine too... in the hospital... \nShe's dying from cancer...")
                     print("You offer your condolences, but an awkward silence falls. \nThe stranger starts to cry quietly and moves away from you...")
                 else:
@@ -27,7 +27,7 @@ def Dialog(start_time):
                 print("You offer your condolences, but an awkward silence falls.\nThe stranger starts to cry quietly and moves away from you...")
         case "happy":
             username = input("The stranger looked at you and asks you in a giddy voice. \n'Hello stranger, what's your name? \n")
-            visit = input("Nice to meet you " + username + " , I'm " + name + ". Where are you going? \n")
+            visit = input("Nice to meet you " + username + ", I'm " + name + ". Where are you going? \n")
             print("The stranger laughs happily. \n'I'm gonna go shopping in the Ginza district!'")
             print(name + " looks around and leans in to whisper... \nI JUST WON THE LOTTERY!!!")
             print(name + "'s phone rings, so they answer it and get to chatting excitedly...")
